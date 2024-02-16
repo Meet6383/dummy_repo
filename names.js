726341f3119ed867bc5b3a49e83cb7a88eb0a44f
Meet6383 <meetp6383@gmail.com.com>
2024-02-16 14:12:24 +0530

    first commit.

---
+++ names.js
@@ -0,0 +1,24 @@
+const secret = "TOP SECRET STUFF";
+const madhav = "madhav";
+const mahir = "mahir";
+
+module.exports = {
+  madhav,
+  mahir,
+};
+
+module.exports.items=["item1","item2"]
+module.exports.person=[{
+    name:"madhav",
+    age:23
+},
+{
+    name:"mahir",
+    age:23
+},
+{
+    name:"mahi",
+    age:23
+}
+]
+console.log(module);
---
+++ utils.js
@@ -0,0 +1,14 @@
+// const sayHi = (names) => {
+//     console.log(`hello ${names}`)
+// } 
+
+// module.exports = sayHi
+
+const num1 = 15
+const num2 = 20
+
+function addValue(){
+    console.log(`The sum of num1 and num2 is : ${num1+num2}`);
+}
+
+addValue()
